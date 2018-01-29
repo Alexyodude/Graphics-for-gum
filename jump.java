80077814734250016afdeccdade03790b7d6e9e4
Alex Park <jihoonhotmail.com@gmail.com>
2018-01-29 16:29:52 +0900

    hit box
    hit

--- jump.java
+++ jump.java
@@ -19,6 +19,7 @@ public class jump extends JPanel implements Runnable, ActionListener, KeyListene
 		double menuwidth = 100;
 		double menuheight = 30;
 		double movement = 0.1;
+		double movement2 = 0.00001;
 		double[] menuposx = new double[numofmenu];
 		double[] menuposy = new double[numofmenu];
 		double t1 = 0;
@@ -39,10 +40,11 @@ public class jump extends JPanel implements Runnable, ActionListener, KeyListene
 		boolean collision = false;
 		boolean menu = false;
 		boolean wall = false;
-		boolean pause = true;
+		boolean pause = false;
 		boolean jump1 = false;
 		boolean left = false;
 		boolean right = false;
+		boolean oops = true;
 		
 		
 		//private List<Bullets> firedBullets = new ArrayList<Bullets>();
@@ -58,7 +60,7 @@ public jump() {
 	x[0] = 500 - sizeofrect1/2;
 	y[0] = 20;
 	//y[0] = ground - (sizeofrect1);
-}
+	}
 /*public void fire(){
     Bullets bullets = new Bullets(x[0], y[0]);
     firedbullets.add(bullets);
@@ -68,17 +70,6 @@ public void paintComponent(Graphics g) {
 	for(int l = 1; l < numofmenu - 3; l++) {
 		x1[l] = (getWidth()/2) - (menuwidth/2);
 		y1[l] = (getHeight()/4) - (menuheight/2) + l * 2 * menuheight;
-	}if(x[4] > 0) {
-		for(int d = 0; d < iop; d++) {
-		x2[d] = x2[d] - d * 10;
-		System.out.println(x2);
-		}
-	}for(int k = 0; k < iop; k++) {
-	y2[k] = 100 * k;
-	}if(x2[4] < 0) {
-		x[4] = getWidth();
-	}for(int s2 = 0; s2 < iop; s2++ ) {
-		s[s2] = 10;
 	}
 	super.paintComponent(g);
 	Graphics2D g2 = (Graphics2D) g;
@@ -91,7 +82,7 @@ public void paintComponent(Graphics g) {
 	g2.fill(new Rectangle2D.Double(x1[i+1] ,y1[i+1] , menuwidth, menuheight));
 	g2.fill(new Rectangle2D.Double(x1[i+2] ,y1[i+2] , menuwidth, menuheight));
 	}
-	g2.fill(new Rectangle2D.Double(100, y[1], s[1], 200));
+	g2.fill(new Rectangle2D.Double(x2[1], y2[1], s[1], s[1]));
 	g2.fill(new Rectangle2D.Double(x2[2], y2[2], s[2], s[2]));
 	g2.fill(new Rectangle2D.Double(x2[3], y2[3], s[3], s[3]));
 	g2.fill(new Rectangle2D.Double(x2[4], y2[4], s[4], s[4]));
@@ -111,35 +102,37 @@ g2.draw(new Line2D.Double(0, ground, getWidth(), ground));
 public void actionPerformed(ActionEvent e) {
 	repaint();
 	}
-public void objforward() {
+/*public void objforward() {
 		if(0 < x2[4]) {
 			for(int h = 0;h < iop; h++) {
 			x2[h] = x2[h] - 1;
-			if(x2[4] < 0) {
+			}if(x2[4] < 0) {
 				x2[4] = 1000;
-				System.out.println("g");
+			}
 		}
-		}
-		}
-	}
+	}*/
 public void keyPressed(KeyEvent e) {
 	int key = e.getKeyCode();
 	//double r = 100 + (y[0] - (y[0] % 100));
 	if(wall == false) {
-	if ((key == KeyEvent.VK_W || key == KeyEvent.VK_SPACE) && collision && pause) {
+	if ((key == KeyEvent.VK_W || key == KeyEvent.VK_SPACE) && collision && !pause) {
 		//System.out.println(uy[0]);
-		uy[0] = uy[0] * (9 * -g * dt);
+		//uy[0] = uy[0] * (9 * -g * dt);
 		y[0] = y[0] - (uy[0] * dt);
-	}else if (key == KeyEvent.VK_A && pause) {
+	}else if (key == KeyEvent.VK_A && !pause) {
     	left = true;
-    }else if (key == KeyEvent.VK_D && pause) {
+    }else if (key == KeyEvent.VK_D && !pause) {
     	right = true;
-    }
-	}if(KeyEvent.VK_P == key && pause) {
-    		pause = false;
-    }if((KeyEvent.VK_P == key) && (pause == false)) {
-    		pause = true;
-   	}
+    }if(KeyEvent.VK_ESCAPE == key && (pause == false)) {
+		pause = true;
+		menu = true;
+    }if((KeyEvent.VK_ESCAPE == key) && (pause == true)) {
+		pause = false;
+		menu = false;
+	}if((KeyEvent.VK_1 == key) && (!pause)) {
+		
+	}
+}
 }
 public void right() {
 	ux[0] = 1;
@@ -149,17 +142,46 @@ public void left() {
 	x[0] = x[0] - movement * dt;
 }
 public void jumping() {
-	if((y[0] <= (ground - sizeofrect1)) && pause) {
+	if((y[0] <= (ground - sizeofrect1)) && !pause) {
 		uy[0] = uy[0] + (-g * dt);
 		y[0] = y[0] - (uy[0] * dt);
 		//t1 = t1 + dt;
-		delay(1);
 		//System.out.println("Time: " + t1 + ", Ypos: " + y[0]+ ", Yvel: " + uy[0]);
 	}/*if(y[0] > (ground - sizeofrect1)) {
 		uy[0] = 0;
 		y[0] = ground - sizeofrect1; 
 		System.out.println(y[0] + "," + uy[0] + "," + (ground - sizeofrect1) + "," + ground);
  	}*/
+	delay(1);
+}
+public void objforward() {
+	/*for(int count = 0; count < iop; count++) {
+		if(x2[count] >= (0 - (s[count]/2))) {
+		x2[count] = ((x2[count]) - movement2);
+	System.out.println(x2[count]);
+		}if(x2[count] <= (0 - (s[count]/2))) {
+			for(int l = 0; l < iop; l++) {
+				x2[l] = 1000;
+			}
+	}
+	}*/
+	for(int count = 0; count < iop; count++) {
+		if(x2[count] < getWidth() + s[count]) {
+			x2[count] = x2[count] - movement2;
+		}if((x2[count] < (0 - (s[count]/2)))) {
+			x2[count] = getWidth()- s[count];
+		}if(x2[count] > getWidth() + s[count]) {
+			x2[count] = 0;
+		}
+	}
+}
+public void bump() {
+	for(int count = 0; count < iop; count++) {
+		if(x[0]+sizeofrect1 > x2[count] && x[0]+sizeofrect1 < x2[count] + s[count] && y[0] > y2[0] && y[0] < y2[4]+s[count]) {
+			right = false;
+		//	x[0] = x[0] - 1;
+		}
+	}
 }
 public void delay(int delay){
 	try{
@@ -177,37 +199,42 @@ public void keyTyped(KeyEvent e) {
 } 
 public void run() {
 	// TODO Auto-generated method stub
-	jumping();
-	objforward();
-	b.run();
+	if(!pause) {
+		objforward();
+		bump();
 	if(left) {
 		left();
 	}if(right) {
 		right();
 	}if(y[0] < (ground - sizeofrect1)) {
 		movement = 40;
+		movement2 = 0.2;
 		collision = false;
-	}else if(y[0] >= (ground - sizeofrect1)) {
-		movement = 0.005;
+		jumping();
+	}if(y[0] >= (ground - sizeofrect1)) {
+		movement = 0.0008;
+		movement2 = 0.00002;
 		collision = true;
 		uy[0] = 60;
-	}if(y[0] - 1.1 >= (ground - sizeofrect1)) {
-		y[0]= 1;
+	}}for(int k = 0; k < iop; k++) {
+		y2[k] = 200 + s[k] * k/2;
+		}for(int s2 = 0; s2 < iop; s2++ ) {
+			s[s2] = 50;
+	}
 	}
 	//System.out.println(y[0] + sizeofrect1);
 	//System.out.println(ground);
 	//System.out.println(x[0] + "," + y[0]);
-}
 
 @Override
 public void mouseClicked(MouseEvent e) {
 	int mouse = e.getButton();
 	if(mouse == 1 && (menu == false) && (getMousePosition().x < x1[0] + menustart1) && (getMousePosition().y < y1[0] + menustart1) && (getMousePosition().x > x1[0]) && (getMousePosition().y > y1[0])) {
 		menu = true;
-		pause = false;
+		pause = true;
 	}else if(mouse == 1 && (menu == true) && (getMousePosition().x < x1[0] + menustart1) && (getMousePosition().y < y1[0] + menustart1) && (getMousePosition().x > x1[0]) && (getMousePosition().y > y1[0])) {
 		menu = false;
-		pause = true;
+		pause = false;
 	} /*if(mouse == 1 && pause) {
 		pause = false;
 		System.out.println(pause + "hi");
